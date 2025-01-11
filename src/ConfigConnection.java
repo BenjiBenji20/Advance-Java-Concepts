8121bedf1ce212e1a20e468eecd297d2e49e7aca
BenjiBenji20 <imperialbenjie393@gmai.com>
2025-01-11 10:48:57 +0800

    This is my first Java to Databse configuration connect (JDBC) using mysql driver

---
+++ src/ConfigConnection.java
@@ -0,0 +1,30 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class ConfigConnection {
+
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://127.0.0.1:3306/jdbc";
+        String user = "root";
+        String password = "";
+
+        try {
+            Connection conn = DriverManager.getConnection(url, user, password);
+            Statement statement = conn.createStatement();
+            ResultSet res = statement.executeQuery("SELECT * FROM users");
+
+            while (res.next()) {
+                System.out.println("ID: " + res.getInt("id")
+                                    + " USERNAME: " + res.getString("username")
+                );
+            }
+
+            conn.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}

38f28c6626303e857f1add3a9bbd810fb1376e70
BenjiBenji20 <imperialbenjie393@gmai.com>
2025-01-15 08:59:38 +0800

    Created files to start basic web app user registration

---
+++ src/main/java/com/connection/config/ConnectionConfig.java
@@ -0,0 +1,27 @@
+package com.connection.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionConfig {
+    private Connection connection;
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public ConnectionConfig() {
+        try {
+            final String url = "jdbc:mysql://127.0.0.1:3306/jdbc";
+            final String user = "root";
+            final String password = "";
+
+            connection = DriverManager.getConnection(url, user, password);
+            System.out.println("Connection established!");
+        }
+        catch (Exception e) {
+            System.out.println("Error found");
+            e.printStackTrace();
+        }
+    }
+}

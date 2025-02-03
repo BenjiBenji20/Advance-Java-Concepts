5cef69875afce497447ef0420e10a703198bb8a3
BenjiBenji20 <imperialbenjie393@gmai.com>
2025-02-03 08:40:40 +0800

    Starting demo project to learn Thymeleaf with Spring web, data-jpa and mysql dependencies

---
+++ src/main/java/com/azathoth/thymeleaf/IntroSpringThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.azathoth.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroSpringThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroSpringThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/azathoth/thymeleaf/IntroSpringThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azathoth.thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroSpringThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

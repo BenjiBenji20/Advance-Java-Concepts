139a9c0130d375d705cf9d6b565727de85f803a3
BenjiBenji20 <imperialbenjie393@gmai.com>
2025-02-04 17:18:13 +0800

    Created a new demo project performing simple CRUD operation for user page

---
+++ sql/spring_demo_db.sql
@@ -0,0 +1,5 @@
+USE spring_demo_db;
+
+SHOW TABLES;
+
+SELECT * FROM product;
---
+++ src/test/java/com/azathoth/SimpleCRUD/SimpleCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azathoth.SimpleCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
